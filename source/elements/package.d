f318107b9c9c95e80fc01da8a507464fea903a77
Preassume <aweraa29@gmail.com>
2021-06-08 12:39:41 -0500

    imports now generated at compile-time

--- source/elements/package.d
+++ source/elements/package.d
@@ -1,14 +1,25 @@
 module elements;
-
 public import elements.element;
-public import elements.air;
-public import elements.sand;
-public import elements.water;
-public import elements.wood;
 
+// To make add a new element, add its name to this array.
+// NOTE: The name of the file and the name of the class MUST match what you add here
+// The menu button and its click events will be auto-generated at compile-time
 enum elementNames = [
 	"air",
 	"sand",
 	"water",
-	"wood"
+	"wood",
+	"acid",
+	"glass",
+	"fire",
+	"steam",
+	"plant",
+	"wacky",
+	"clone",
+	"smoke",
 ];
+
+// import all modules with names matching the elementNames contents
+static foreach(e; elementNames){
+	mixin("public import elements." ~ e ~ ';');
+}
